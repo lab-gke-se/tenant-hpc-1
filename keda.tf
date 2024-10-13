f6b382057e051d071959f9488b512a90bf0d003d
Dave Butland <dgbutland@gmail.com>
2024-10-13 11:53:45 +0100

    Turned off autoscaling in hpc-1-standard-1

--- keda.tf
+++ keda.tf
@@ -13,9 +13,30 @@ resource "google_project_iam_member" "keda_monitoring_viewer" {
   member  = "serviceAccount:sa-keda-operator@lab-gke-se.iam.gserviceaccount.com"
 }
 
-resource "google_project_iam_member" "keda_iam_workloadIdentityUser" {
+resource "google_service_account_iam_member" "keda_iam_workloadIdentityUser" {
+  service_account_id = module.keda_service_account.id
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:lab-gke-se.svc.id.goog[keda/keda-operator]"
+}
+
+module "testapp_service_account" {
+  source = "github.com/lab-gke-se/modules//iam/service_account?ref=0.0.4"
+
+  project      = local.project
+  name         = "testapp"
+  display_name = "Test App Keda Service Account"
+  description  = "Test App"
+}
+
+resource "google_project_iam_member" "testapp_pubsub_subscriber" {
   project = local.project
-  role    = "roles/iam.workloadIdentityUser"
-  member  = "serviceAccount:sa-keda-operator@lab-gke-se.iam.gserviceaccount.com"
+  role    = "roles/pubsub.subscriber"
+  member  = "serviceAccount:testapp@lab-gke-se.iam.gserviceaccount.com"
+}
+
+resource "google_service_account_iam_member" "testapp_iam_workloadIdentityUser" {
+  service_account_id = module.testapp_service_account.id
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:lab-gke-se.svc.id.goog[default/testapp]"
 }
 
--- storage.tf
+++ storage.tf
@@ -1,8 +1,18 @@
+module "storage_east4" {
+  source = "github.com/lab-gke-se/modules//storage/bucket?ref=0.0.4"
+
+  project             = local.project
+  name                = "hpc-1-bucket-1"
+  location            = "us-east4"
+  data_classification = "data"
+  kms_key_id          = local.substitutions.kms_key
+}
+
 module "storage" {
   source = "github.com/lab-gke-se/modules//storage/bucket?ref=0.0.4"
 
   project             = local.project
-  name                = "hpc-1-storage"
+  name                = "hpc-1-bucket-3"
   location            = "us-central1"
   data_classification = "data"
   kms_key_id          = local.substitutions.kms_key_usc1
@@ -16,4 +26,23 @@ module "storage_source" {
   location            = "us-central1"
   data_classification = "source"
   kms_key_id          = local.substitutions.kms_key_usc1
+}
+
+# resource "google_storage_bucket_iam_member" "testapp_storage_admin" {
+#   bucket = module.storage.name
+#   role   = "roles/storage.admin"
+#   member = "principal://iam.googleapis.com/projects/52991355109/locations/global/workloadIdentityPools/lab-gke-se.svc.id.goog/subject/ns/default/sa/default"
+# }
+
+
+resource "google_project_iam_member" "default_storage_admin" {
+  project = local.project
+  role    = "roles/storage.admin"
+  member  = "principal://iam.googleapis.com/projects/52991355109/locations/global/workloadIdentityPools/lab-gke-se.svc.id.goog/subject/ns/default/sa/default"
+}
+
+resource "google_project_iam_member" "node_storage_admin" {
+  project = local.project
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:hpc-1-nodes@lab-gke-se.iam.gserviceaccount.com"
 }
