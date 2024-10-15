7abb980faafd1540caf0ebbc6586d44a0fb43d66
Dave Butland <dgbutland@gmail.com>
2024-10-15 15:02:44 +0100

    Re-enabled autoscaling on hpc-1-standard-3

--- storage.tf
+++ storage.tf
@@ -18,6 +18,33 @@ module "storage" {
   kms_key_id          = local.substitutions.kms_key_usc1
 }
 
+module "parallelstore" {
+  source   = "../modules/parallelstore"
+  for_each = local.parallelstore_configs
+
+  name                 = each.value.name
+  capacityGib          = each.value.capacityGib
+  description          = try(each.value.description, null)
+  network              = try(each.value.network, null)
+  reservedIpRange      = try(each.value.reservedIpRange, null)
+  fileStripeLevel      = try(each.value.fileStripeLevel, null)
+  directoryStripeLevel = try(each.value.directoryStripeLevel, null)
+  labels               = try(each.value.labels, null)
+}
+
+resource "google_project_service_identity" "parallelstore" {
+  provider = google-beta
+
+  project = local.project
+  service = "parallelstore.googleapis.com"
+}
+
+resource "google_project_iam_member" "parallel_storage_admin" {
+  project = local.project
+  role    = "roles/storage.admin"
+  member  = google_project_service_identity.parallelstore.member
+}
+
 module "storage_source" {
   source = "github.com/lab-gke-se/modules//storage/bucket?ref=0.0.4"
 
@@ -31,7 +58,7 @@ module "storage_source" {
 # resource "google_storage_bucket_iam_member" "testapp_storage_admin" {
 #   bucket = module.storage.name
 #   role   = "roles/storage.admin"
-#   member = "principal://iam.googleapis.com/projects/52991355109/locations/global/workloadIdentityPools/lab-gke-se.svc.id.goog/subject/ns/default/sa/default"
+#   member = "principal://iam.googleapis.com/projects/52991355109/locations/global/workloadIdentityPools/lab-gke-se.svc.id.goog/subject/ns/default/sa/pass"
 # }
 
 
@@ -46,3 +73,4 @@ resource "google_project_iam_member" "node_storage_admin" {
   role    = "roles/storage.admin"
   member  = "serviceAccount:hpc-1-nodes@lab-gke-se.iam.gserviceaccount.com"
 }
+
