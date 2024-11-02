afbbd7ca46a40215d628b8f5bd9639fe0d6bf9b1
Dave Butland <dgbutland@gmail.com>
2024-11-02 01:03:59 +0000

    Removed version

--- version.tf
+++ version.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-    google = {
-      version = "< 5.44.0"
-    }
+    # google = {
+    #   version = "< 5.44.0"
+    # }
     # google-beta = {
     #   version = "= 5.33.0"
     # }
